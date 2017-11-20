4971022fbde9fa8527b6dfa4e52b53c83913d795
Luan Nguyen <huuluanpy@gmail.com>
2017-11-20 23:41:16 +0700

    201 kiem tra request, tao field password

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
 use App\User;
 use App\Role;
 
@@ -39,7 +40,7 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UserRequest $request)
     {
         //
 
