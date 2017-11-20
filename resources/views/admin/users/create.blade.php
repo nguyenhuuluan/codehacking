41cb880ff0c600da042a4dda3692d84fe384f80c
Luan Nguyen <huuluanpy@gmail.com>
2017-11-20 23:07:19 +0700

    198 Testing form and creating form fields

---
+++ resources/views/admin/users/create.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.admin')
+
+@section('content')
+
+	<h1>Create User</h1>
+
+	{!! Form::open(['method'=>'POST', 'action'=>'AdminUsersController@store']) !!}
+	<div class="form-group">
+		{!! Form::label('name', 'Name:') !!}
+		{!! Form::text('name', null, ['class'=>'form-control']) !!}
+
+	</div>
+
+	<div class="form-group">
+		{!! Form::label('email', 'Email:') !!}
+		{!! Form::email('email', null, ['class'=>'form-control']) !!}
+
+	</div>
+
+	<div class="form-group">
+		{!! Form::label('role_id', 'Role:') !!}
+		{!! Form::text('role_id', null, ['class'=>'form-control']) !!}
+
+	</div>
+
+	<div class="form-group">
+		{!! Form::label('status', 'Status:') !!}
+		{!! Form::select('status', array(1=>'Active', 0=>'Not Active'), 1, ['class'=>'form-control']) !!}
+
+	</div>
+	
+	<div class="form-group">
+		{!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
+	</div>
+
+	{!! Form::close() !!}
+
+
+
+
+@endsection
