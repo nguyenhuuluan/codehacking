c5a811a663989872d1daf33dbc908ee2ed8515e0
Luan Nguyen <huuluanpy@gmail.com>
2017-11-21 00:00:27 +0700

    203 Them cot path cho user, them field upload file

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -4,35 +4,36 @@
 
 	<h1>Create User</h1>
 
-	{!! Form::open(['method'=>'POST', 'action'=>'AdminUsersController@store']) !!}
+	{!! Form::open(['method'=>'POST', 'action'=>'AdminUsersController@store', 'files'=>true]) !!}
 	<div class="form-group">
 		{!! Form::label('name', 'Name:') !!}
 		{!! Form::text('name', null, ['class'=>'form-control']) !!}
-
 	</div>
 
 	<div class="form-group">
 		{!! Form::label('email', 'Email:') !!}
 		{!! Form::email('email', null, ['class'=>'form-control']) !!}
-
 	</div>
 
 	<div class="form-group">
 		{!! Form::label('role_id', 'Role:') !!}
 		{!! Form::select('role_id', [''=>'Choose Options'] + $roles ,null, ['class'=>'form-control']) !!}
-
 	</div>
 
 	<div class="form-group">
 		{!! Form::label('status', 'Status:') !!}
 		{!! Form::select('status', array(1=>'Active', 0=>'Not Active'), 1, ['class'=>'form-control']) !!}
-
 	</div>
+
+	<div class="form-group">
+		{!! Form::label('file', 'File:') !!}
+		{!! Form::file('file', null, ['class'=>'form-control']) !!}
+	</div>
+
 
 	<div class="form-group">
 		{!! Form::label('password', 'Password:') !!}
 		{!! Form::password('password', ['class'=>'form-control']) !!}
-
 	</div>
 	
 	<div class="form-group">
