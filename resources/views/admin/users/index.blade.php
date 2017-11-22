579752c4a0a62930b64fa666a2a8b8aace87c35e
Luan Nguyen <huuluanpy@gmail.com>
2017-11-23 00:45:00 +0700

    210 Edit user: action va hien thi thong tin user edit

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -30,7 +30,7 @@
 		<tr>
 			<td>{{ $user->id }}</td>
 			<td><img height ="50" src="{{ $user->photo ? $user->photo->file :'no user photo' }}"></td>
-			<td>{{ $user->name }}</td>
+			<td><a href="{{ route('users.edit', $user->id) }}">{{ $user->name }}</a></td>
 			<td>{{ $user->email }}</td>
 			<td>{{ $user->role->name }}</td>
 			<td>{{ $user->is_active == 1 ? "Active" : 'Not Active' }}</td>
