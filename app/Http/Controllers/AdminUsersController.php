e47b3fa04732855d4fba3f912848689aae59caf9
Luan Nguyen <huuluanpy@gmail.com>
2017-11-27 22:23:04 +0700

    219 Xoa hinh

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -158,7 +158,18 @@ class AdminUsersController extends Controller
     public function destroy($id)
     {
         //
-        User::findOrFail($id)->delete();
+        //User::findOrFail($id)->delete();
+        $user = User::findOrFail($id);
+
+        unlink(public_path().$user->photo->file);
+
+
+
+        $user->delete();
+
+
+
+
 
         Session::flash('deleted_user','The user has been deleted');
 
